4514a44c0ceecf25f7fd5e1ac67c0bdfdf0e0dd9
Vladimir Kornev <kvv@pckvv.(none)>
2012-07-07 19:20:23 +0700

    Create 3D quadrocopter model

---
+++ k-control/src/main/java/ru/kcode/view/copter/Copetr3dPanel.java
@@ -0,0 +1,53 @@
+package ru.kcode.view.copter;
+
+import java.awt.Dimension;
+
+import javax.media.opengl.GLCapabilities;
+import javax.media.opengl.GLProfile;
+import javax.media.opengl.awt.GLCanvas;
+import javax.swing.JPanel;
+
+import com.jogamp.opengl.util.FPSAnimator;
+
+public class Copetr3dPanel extends JPanel {
+    private static final long serialVersionUID = 2219431191224761363L;
+    
+    private FPSAnimator animator;
+    private GLCanvas canvas;
+    
+    public Copetr3dPanel() {
+        
+        initComponents();
+
+        animator = new FPSAnimator(40);
+        Copter3dView copter3d = new Copter3dView();
+
+        canvas.addGLEventListener(copter3d);
+        
+        animator.add(canvas);
+
+
+        // This is a workaround for the GLCanvas not adjusting its size, when the frame is resized.
+        canvas.setMinimumSize(new Dimension());  
+        animator.start();       
+        
+    }
+    
+    private void initComponents() {
+        jogamp.common.Debug.debugAll();
+        canvas = new GLCanvas(createGLCapabilites());
+        canvas.setBounds(10, 10, 400, 400);
+        add(canvas);
+    }
+    
+    private GLCapabilities createGLCapabilites() {
+        GLCapabilities capabilities = new GLCapabilities(GLProfile.getGL2GL3());
+        capabilities.setHardwareAccelerated(true);
+
+        // try to enable 2x anti aliasing - should be supported on most hardware
+        capabilities.setNumSamples(2);
+        capabilities.setSampleBuffers(true);
+        
+        return capabilities;
+    }
+}
---
+++ k-control/src/main/java/ru/kcode/view/copter/Copter3dView.java
@@ -0,0 +1,195 @@
+package ru.kcode.view.copter;
+
+import javax.media.opengl.GL;
+import javax.media.opengl.GL2;
+import javax.media.opengl.GLAutoDrawable;
+import javax.media.opengl.GLEventListener;
+import javax.media.opengl.glu.GLU;
+
+public class Copter3dView implements GLEventListener {
+    private static final float QC_SHAFT_LENGTH = 1f;
+    private static final float QC_SHAFT_WIDTH = 0.2f;
+    private static final float QC_ROTOR_RADIUS = 0.5f;
+    private static final float QC_SHAFT_LENGTH_DELTA = (float) Math.sqrt((QC_SHAFT_LENGTH * QC_SHAFT_LENGTH) / 2f);
+    private static final float QC_SHAFT_WIDTH_DELTA = (float) Math.sqrt((QC_SHAFT_WIDTH * QC_SHAFT_WIDTH) / 2f);
+    private static final float QC_ROTOR_DELTA = (float) Math.sqrt(
+            ( (QC_SHAFT_LENGTH + QC_ROTOR_RADIUS) * (QC_SHAFT_LENGTH + QC_ROTOR_RADIUS) ) / 2f );
+
+    private GLU glu;
+    private int w, h;
+    private float angle = 0.0f;
+
+    @Override
+    public void init(GLAutoDrawable drawable) {
+        w = drawable.getWidth();
+        h = drawable.getHeight();
+
+        GL2 gl = (GL2) drawable.getGL();
+        glu = new GLU();
+
+        gl.glShadeModel(GL2.GL_SMOOTH);
+        gl.glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+        gl.glClearDepth(1.0f);
+        gl.glEnable(GL.GL_DEPTH_TEST);
+        gl.glDepthFunc(GL.GL_LEQUAL);
+        gl.glHint(GL2.GL_PERSPECTIVE_CORRECTION_HINT, GL.GL_NICEST);
+
+    }
+
+    @Override
+    public void dispose(GLAutoDrawable drawable) {
+
+    }
+
+    @Override
+    public void display(GLAutoDrawable drawable) {
+        GL2 gl = (GL2) drawable.getGL();
+
+        w = drawable.getWidth();
+        h = drawable.getHeight();
+        gl.glMatrixMode(GL2.GL_MODELVIEW);
+        gl.glLoadIdentity();
+        gl.glViewport(0, 0, w, h);
+        gl.glMatrixMode(GL2.GL_PROJECTION);
+        gl.glLoadIdentity();
+        glu.gluPerspective(45.0f, (float) w / (float) h, 0.1f, 100.0f);
+
+        gl.glClear(GL.GL_COLOR_BUFFER_BIT | GL.GL_DEPTH_BUFFER_BIT);
+
+        drawScene(drawable);
+
+    }
+
+    @Override
+    public void reshape(GLAutoDrawable drawable, int x, int y, int width, int height) {
+        
+    }
+
+    public void drawScene(GLAutoDrawable drawable) {
+        GL2 gl = (GL2) drawable.getGL();
+
+        angle++;
+
+        gl.glTranslatef(0f, 0f, -7f);
+        gl.glRotatef(angle, 1f, 1f, 1f);
+        //gl.glRotatef(0, 0f, 1f, 0f);
+        //gl.glRotatef(0, 0f, 0f, 1f);
+
+        drawCopter(drawable);
+    }
+
+    public void drawCopter(GLAutoDrawable drawable) {
+        drawAxis(drawable);
+        
+        drawShaft(drawable, -1, -1);
+        drawProp(drawable, -1, -1);
+        
+        drawShaft(drawable, 1, -1);
+        drawProp(drawable, 1, -1);
+        
+        drawShaft(drawable, -1, 1);
+        drawProp(drawable, -1, 1);
+        
+        drawShaft(drawable, 1, 1);
+        drawProp(drawable, 1, 1);
+    }
+
+    private void drawAxis(GLAutoDrawable drawable) {
+        GL2 gl = (GL2) drawable.getGL();
+        double cx = 0;
+        double cy = 0;
+        double cz = 0;
+
+        gl.glBegin(GL2.GL_LINE_STRIP);
+        gl.glColor4f(1, 0, 0, 1);
+        gl.glVertex3d(cx-0.2, cy, cz);
+        gl.glVertex3d(cx+1, cy, cz);
+        gl.glEnd();
+        
+        gl.glBegin(GL2.GL_LINE_STRIP);
+        gl.glColor4f(0, 1, 0, 1);
+        gl.glVertex3d(cx, cy-0.2, cz);
+        gl.glVertex3d(cx, cy+1, cz);
+        gl.glEnd();
+        
+        gl.glBegin(GL2.GL_LINE_STRIP);
+        gl.glColor4f(0, 0, 1, 1);
+        gl.glVertex3d(cx, cy, cz-0.2);
+        gl.glVertex3d(cx, cy, cz+1);
+        gl.glEnd();
+
+    }
+    
+    private void drawProp(GLAutoDrawable drawable, float xShift, float zShift) {
+        GL2 gl = (GL2) drawable.getGL();
+        float increment = (float) (2 * Math.PI / 50);
+        float cx = 0;
+        float cy = 0;
+        float cz = 0;
+        float radius = QC_ROTOR_RADIUS;
+        
+        if (zShift > 0) {
+            gl.glColor4f(0, 0, 1, 1);
+        }
+        else {
+            gl.glColor4f(1, 0, 0, 1);
+        }
+
+        for (float angle = 0; angle < 2 * Math.PI; angle += increment) {
+            
+            gl.glBegin(GL2.GL_POLYGON);
+            
+            gl.glVertex3d(cx + Math.cos(angle) * radius+(QC_ROTOR_DELTA)*xShift,
+                    cy - QC_SHAFT_WIDTH,
+                    cz + Math.sin(angle) * radius+(QC_ROTOR_DELTA)*zShift);
+            gl.glVertex3d(cx + Math.cos(angle) * radius+(QC_ROTOR_DELTA)*xShift,
+                    cy + QC_SHAFT_WIDTH,
+                    cz + Math.sin(angle) * radius+(QC_ROTOR_DELTA)*zShift);
+            gl.glVertex3d(cx + Math.cos(angle + increment) * radius+(QC_ROTOR_DELTA)*xShift,
+                    cy + QC_SHAFT_WIDTH,
+                    cz + Math.sin(angle + increment) * radius+(QC_ROTOR_DELTA)*zShift);
+            gl.glVertex3d(cx + Math.cos(angle + increment) * radius+(QC_ROTOR_DELTA)*xShift,
+                    cy - QC_SHAFT_WIDTH,
+                    cz + Math.sin(angle + increment) * radius+(QC_ROTOR_DELTA)*zShift);
+
+            gl.glEnd();
+        }
+    }
+
+    private void drawShaft(GLAutoDrawable drawable, float xShift, float zShift) {
+        GL2 gl = (GL2) drawable.getGL();
+        float cx = 0;
+        float cy = 0;
+        float cz = 0;
+        if (zShift > 0) {
+            gl.glColor4f(0, 0, 1, 1);
+        }
+        else {
+            gl.glColor4f(1, 0, 0, 1);
+        }
+
+        gl.glBegin(GL2.GL_QUADS);
+
+        gl.glVertex3d(cx+(QC_SHAFT_WIDTH_DELTA)*xShift, cy+QC_SHAFT_WIDTH/2, cz);
+        gl.glVertex3d(cx+(QC_SHAFT_WIDTH_DELTA+QC_SHAFT_LENGTH_DELTA)*xShift, cy+QC_SHAFT_WIDTH/2, cz+(QC_SHAFT_LENGTH_DELTA)*zShift);
+        gl.glVertex3d(cx+(QC_SHAFT_LENGTH_DELTA)*xShift, cy+QC_SHAFT_WIDTH/2, cz+(QC_SHAFT_LENGTH_DELTA+QC_SHAFT_WIDTH_DELTA)*zShift);
+        gl.glVertex3d(cx, cy+QC_SHAFT_WIDTH/2, cz+(QC_SHAFT_WIDTH_DELTA)*zShift);
+        
+        gl.glVertex3d(cx, cy+QC_SHAFT_WIDTH/2, cz+(QC_SHAFT_WIDTH_DELTA)*zShift);
+        gl.glVertex3d(cx+(QC_SHAFT_LENGTH_DELTA)*xShift, cy+QC_SHAFT_WIDTH/2, cz+(QC_SHAFT_LENGTH_DELTA+QC_SHAFT_WIDTH_DELTA)*zShift);
+        gl.glVertex3d(cx+(QC_SHAFT_LENGTH_DELTA)*xShift, cy-QC_SHAFT_WIDTH/2, cz+(QC_SHAFT_LENGTH_DELTA+QC_SHAFT_WIDTH_DELTA)*zShift);
+        gl.glVertex3d(cx, cy-QC_SHAFT_WIDTH/2, cz+(QC_SHAFT_WIDTH_DELTA)*zShift);
+        
+        gl.glVertex3d(cx+(QC_SHAFT_WIDTH_DELTA)*xShift, cy-QC_SHAFT_WIDTH/2, cz);
+        gl.glVertex3d(cx+(QC_SHAFT_WIDTH_DELTA+QC_SHAFT_LENGTH_DELTA)*xShift, cy-QC_SHAFT_WIDTH/2, cz+(QC_SHAFT_LENGTH_DELTA)*zShift);
+        gl.glVertex3d(cx+(QC_SHAFT_LENGTH_DELTA)*xShift, cy-QC_SHAFT_WIDTH/2, cz+(QC_SHAFT_LENGTH_DELTA+QC_SHAFT_WIDTH_DELTA)*zShift);
+        gl.glVertex3d(cx, cy-QC_SHAFT_WIDTH/2, cz+(QC_SHAFT_WIDTH_DELTA)*zShift);
+
+        gl.glVertex3d(cx+(QC_SHAFT_WIDTH_DELTA)*xShift, cy+QC_SHAFT_WIDTH/2, cz);
+        gl.glVertex3d(cx+(QC_SHAFT_WIDTH_DELTA+QC_SHAFT_LENGTH_DELTA)*xShift, cy+QC_SHAFT_WIDTH/2, cz+(QC_SHAFT_LENGTH_DELTA)*zShift);
+        gl.glVertex3d(cx+(QC_SHAFT_WIDTH_DELTA+QC_SHAFT_LENGTH_DELTA)*xShift, cy-QC_SHAFT_WIDTH/2, cz+(QC_SHAFT_LENGTH_DELTA)*zShift);
+        gl.glVertex3d(cx+(QC_SHAFT_WIDTH_DELTA)*xShift, cy-QC_SHAFT_WIDTH/2, cz);
+
+        gl.glEnd();
+    }
+}
